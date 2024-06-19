bc2f60bca50ab3cd835acfb5a07e356dd7c8d00a
Stalaka <stephanie.ochoka@strtahmore.edu>
2024-06-19 22:39:56 +0300

    commit

--- Sign Up.php
+++ Sign Up.php
@@ -1,8 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-    <body>
-    <?php
+<?php
 require_once("includes/db_connect.php");
+include_once("templates/header.php");
+include_once("templates/nav.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $fullname = mysqli_real_escape_string($conn, $_POST["fullname"]);
@@ -29,41 +28,46 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $conn->close();
 }
 ?>
-
-    <div class="container my-4">
-    <a href="index.php">Home</a>
-    <a href="about.php">About Us</a>
-    <a href="signup.php">Sign Up</a>
-    <a href="login.php">Log In</a>
-    <a href="contact.php">Contact Us</a>
-    <a href="impact.php">Our Impact</a>
-</div>
-<?php
-include_once("templates/header.php");
-include_once("templates/nav.php");
-?>
-
-        <h1>Sign Up</h1>
-        <body style="background-color: turquoise;"></body>
-        <style>
-            h1{
-                color: black;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+    <style>
+        h1 {
+            color: black;
             text-transform: uppercase;
             text-decoration: wavy;
         }
-        </style>
-        <form action="signup_action.php" method="post">
-    <label for="Fn">Fullname:</label><br>
-    <input type="text" id="Fn" name="fullname" placeholder="Fullname" required><br><br>
-    <input type="text" name="gender" placeholder="Gender" required><br><br>
-    <input type="password" name="password" placeholder="Password" required><br><br>
-    <input type="email" name="email" placeholder="Email Address" required><br><br>
-    <input type="number" name="form" placeholder="Form" required><br><br>
-    <input type="text" name="school" placeholder="School" required><br><br>
-    <input type="text" name="phone" placeholder="Phone Number" required><br><br>
-    <input type="submit" class="btn btn-default" value="Sign Up">
-</form>
+        body {
+            background-color: turquoise;
+        }
+    </style>
+</head>
+<body>
+    <div class="container my-4">
+        <a href="index.php">Home</a>
+        <a href="about.php">About Us</a>
+        <a href="signup.php">Sign Up</a>
+        <a href="login.php">Log In</a>
+        <a href="contact.php">Contact Us</a>
+        <a href="impact.php">Our Impact</a>
+    </div>
 
-<?php include_once("templates/footer.php"); ?>
-    </body>
+    <h1>Sign Up</h1>
+    <form action="signup.php" method="post">
+        <label for="Fn">Fullname:</label><br>
+        <input type="text" id="Fn" name="fullname" placeholder="Fullname" required><br><br>
+        <input type="text" name="gender" placeholder="Gender" required><br><br>
+        <input type="password" name="password" placeholder="Password" required><br><br>
+        <input type="email" name="email" placeholder="Email Address" required><br><br>
+        <input type="number" name="form" placeholder="Form" required><br><br>
+        <input type="text" name="school" placeholder="School" required><br><br>
+        <input type="text" name="phone" placeholder="Phone Number" required><br><br>
+        <input type="submit" class="btn btn-default" value="Sign Up">
+    </form>
+
+    <?php include_once("templates/footer.php"); ?>
+</body>
 </html>
