f76a3ee30bd8c5c5c66ec0ceaa84fd4196cc0cc8
Stalaka <stephanie.ochoka@strtahmore.edu>
2024-06-28 22:45:05 +0300

    commit

---
+++ images/templates/nav.php
@@ -0,0 +1,11 @@
+<div class="topnav">
+    <a href="./">Home</a>
+    <a href="about.php">About Us</a>
+    <a href="impact.php">Our Impact</a>
+    <a href="contact.php">Contact Us</a>
+    
+    <div class="topnav-right">
+        <a href="Sign Up.php">Sign Up</a>
+        <a href="Log In.php">Sign In</a>
+    </div>
+    
