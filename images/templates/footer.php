640c414f441236ea4c7aca6331db9d0900121e86
Stalaka <stephanie.ochoka@strtahmore.edu>
2024-06-28 23:41:11 +0300

    commit

--- images/templates/footer.php
+++ images/templates/footer.php
@@ -1,5 +1,17 @@
 <div class="footer">
-            Copyright &copy; <?php print date("Y"); ?> - Allrights Reserved - BBIT 2.1 Group C
+        <div class="container">
+            <div class="footer-nav">
+                <a href="index.php">Home</a>
+                <a href="about.php">About Us</a>
+                <a href="projects.php">Projects</a>
+                <a href="products.php">Products</a>
+                <a href="contact.php">Contact Us</a>
+                <a href="impact.php">Our Impact</a>
+                <a href="signup.php">Sign Up</a>
+                <a href="login.php">Log In</a>
+            </div>
+            <p>Copyright &copy; <?php echo date("Y"); ?> - All rights Reserved - My Webpage</p>
         </div>
+    </div>
     </body>
 </html>
