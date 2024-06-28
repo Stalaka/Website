9f23cf318764ba7e27eff56667655b333077a73e
Stalaka <stephanie.ochoka@strtahmore.edu>
2024-06-28 23:16:56 +0300

    commit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-include_once("templates/header.php");
-include_once("templates/nav.php");
+include_once("db_connect.php");
+include_once("header.php");
 ?>
 
 <div class="container my-4">
@@ -24,40 +24,22 @@ include_once("templates/nav.php");
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>Service 1</td>
-                <td>Description of service 1</td>
-                <td>Ksh.500</td>
-            </tr>
-            <tr>
-                <td>Service 2</td>
-                <td>Description of service 2</td>
-                <td>Ksh.1000</td>
-            </tr>
-            <tr>
-                <td>Service 3</td>
-                <td>Description of service 3</td>
-                <td>Ksh.1500</td>
-            </tr>
+            <?php
+            $sql = "SELECT name, description, price FROM services";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                    echo "<tr><td>{$row['name']}</td><td>{$row['description']}</td><td>Ksh.{$row['price']}</td></tr>";
+                }
+            } else {
+                echo "<tr><td colspan='3'>No services available</td></tr>";
+            }
+            ?>
         </tbody>
     </table>
 
     <h2>Service Details Form</h2>
     <form>
         <div class="mb-3">
-            <label for="serviceName" class="form-label">Service Name</label>
-            <input type="text" class="form-control" id="serviceName" placeholder="Enter service name">
-        </div>
-        <div class="mb-3">
-            <label for="serviceDescription" class="form-label">Service Description</label>
-            <textarea class="form-control" id="serviceDescription" rows="3" placeholder="Enter service description"></textarea>
-        </div>
-        <div class="mb-3">
-            <label for="servicePrice" class="form-label">Service Price</label>
-            <input type="number" class="form-control" id="servicePrice" placeholder="Enter service price">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-</div>
-
-<?php include_once("templates/footer.php"); ?>
+            <label for="serviceName" class
