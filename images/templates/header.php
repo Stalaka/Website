7b8862bbfa1dcab0073fa32130f4b2918834da56
Stalaka <stephanie.ochoka@strtahmore.edu>
2024-06-28 23:33:15 +0300

    commit

--- images/templates/header.php
+++ images/templates/header.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8" >
-        <title>Website</title>
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-    <body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to My Webpage</title>
+    <!--Bootstrap CSS-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <?php include_once("nav.php"); ?>
