00b0a36e769dd26a121e8d028a16d9d9e0900d50
Stalaka <stephanie.ochoka@strtahmore.edu>
2024-06-28 23:23:26 +0300

    commit

---
+++ images/templates/side_bar.php
@@ -0,0 +1,53 @@
+<div class="sidebar">
+    <h2>Quick Links</h2>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About Us</a></li>
+        <li><a href="projects.php">Projects</a></li>
+        <li><a href="products.php">Products</a></li>
+        <li><a href="contact.php">Contact Us</a></li>
+        <li><a href="impact.php">Our Impact</a></li>
+        <li><a href="signup.php">Sign Up</a></li>
+        <li><a href="login.php">Log In</a></li>
+    </ul>
+    
+    <h2>Recent Posts</h2>
+    <ul>
+        <li><a href="#">Post Title 1</a></li>
+        <li><a href="#">Post Title 2</a></li>
+        <li><a href="#">Post Title 3</a></li>
+        <li><a href="#">Post Title 4</a></li>
+    </ul>
+    
+    <h2>Contact Info</h2>
+    <p>Call Us: +2547564321777</p>
+    <p>Email: saproache@gmail.com</p>
+</div>
+
+<style>
+    .sidebar {
+        width: 250px;
+        padding: 15px;
+        background-color: #f4f4f4;
+        border: 1px solid #ccc;
+    }
+    .sidebar h2 {
+        font-size: 18px;
+        margin-bottom: 10px;
+        color: #333;
+    }
+    .sidebar ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    .sidebar ul li {
+        margin-bottom: 10px;
+    }
+    .sidebar ul li a {
+        text-decoration: none;
+        color: #007BFF;
+    }
+    .sidebar ul li a:hover {
+        text-decoration: underline;
+    }
+</style>
